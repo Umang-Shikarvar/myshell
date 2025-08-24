be185243d842385ba3346383c0f044cdd459c643
Umang Shikarvar <umang.shikarvar@iitgn.ac.in>
2025-08-25 01:45:53 +0530

    added going back to parent directory in cd

--- builtin.c
+++ builtin.c
@@ -4,7 +4,48 @@ void cd(char** args,char** path,const char* base_path)
 {
     if (args[1])
     {
-        if (chdir(args[1])!=0)
+        if(strcmp(args[1],"~")==0)
+        {
+            chdir(base_path);
+            *path="~";
+        }
+        else if(strcmp(args[1],"..")==0)
+        {
+            if(*path==NULL)
+            {
+                perror("getcwd failed");
+            }
+            else if(strcmp(*path,base_path)!=0)
+            {
+                if(chdir("..")!=0)
+                {
+                    perror("chdir failed");
+                }
+                else
+                {
+                    *path=getcwd(NULL, 0);
+                    if(*path==NULL)
+                    {
+                        perror("getcwd failed");
+                    }
+                    else
+                    {
+                        char* temp=*path+strlen(base_path);
+                        free(*path);
+                        if(temp)
+                        {
+                            temp[0]='~';
+                            *path=temp;
+                        }
+                        else
+                        {
+                            *path="~";
+                        }
+                    }
+                }
+            }
+        }
+        else if (chdir(args[1])!=0)
         {
             perror("chdir failed");
         }
@@ -41,7 +82,6 @@ void cd(char** args,char** path,const char* base_path)
     else
     {
         chdir(base_path);
-        free(*path);
         *path="~";
     }
 }
